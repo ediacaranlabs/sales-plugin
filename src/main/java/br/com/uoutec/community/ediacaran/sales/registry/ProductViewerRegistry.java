d6f7873c4e529d5fa5b27a05f0b922d7e2b81380
Brandao <Brandao@git.uoutec.com.br>
2025-02-18 11:36:53 -0300

    default

--- src/main/java/br/com/uoutec/community/ediacaran/sales/registry/ProductViewerRegistry.java
+++ src/main/java/br/com/uoutec/community/ediacaran/sales/registry/ProductViewerRegistry.java
@@ -2,6 +2,7 @@ package br.com.uoutec.community.ediacaran.sales.registry;
 
 import java.util.List;
 
+import br.com.uoutec.community.ediacaran.front.pub.widget.Widget;
 import br.com.uoutec.community.ediacaran.sales.pub.ProductViewerHandler;
 
 public interface ProductViewerRegistry {
@@ -9,8 +10,14 @@ public interface ProductViewerRegistry {
 	void registerProductViewerHandler(ProductViewerHandler handler) throws ProductViewerRegistryException;
 	
 	void unregisterProductViewerHandler(String id) throws ProductViewerRegistryException;
+
+	void registerProductViewerWidget(Widget widget) throws ProductViewerRegistryException;
 	
+	void unregisterProductViewerWidget(String id) throws ProductViewerRegistryException;
+
 	List<ProductViewerHandler> getProductViewerHandlers();
+	
+	List<Widget> getProductViewerWidgets();
 	
 	ProductViewerHandler getProductViewerHandler();
 	
