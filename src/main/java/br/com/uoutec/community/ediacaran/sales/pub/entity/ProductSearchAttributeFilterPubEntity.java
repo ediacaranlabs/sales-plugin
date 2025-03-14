656efeebf545a99b7263ad4444d3108e6503e37d
Brandao <Brandao@git.uoutec.com.br>
2025-03-14 11:51:49 -0300

    default

---
+++ src/main/java/br/com/uoutec/community/ediacaran/sales/pub/entity/ProductSearchAttributeFilterPubEntity.java
@@ -0,0 +1,8 @@
+package br.com.uoutec.community.ediacaran.sales.pub.entity;
+
+import br.com.uoutec.community.ediacaran.sales.entity.ProductSearchFilter;
+import br.com.uoutec.pub.entity.AbstractPubEntity;
+
+public class ProductSearchAttributeFilterPubEntity extends AbstractPubEntity<ProductSearchAttributeFilter> {
+
+}
---
+++ src/main/java/br/com/uoutec/community/ediacaran/sales/pub/entity/ProductSearchFilterPubEntity.java
@@ -0,0 +1,88 @@
+package br.com.uoutec.community.ediacaran.sales.pub.entity;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import javax.validation.constraints.NotNull;
+
+import org.brandao.brutos.annotation.Basic;
+import org.brandao.brutos.annotation.MappingTypes;
+import org.brandao.brutos.annotation.Transient;
+
+import br.com.uoutec.community.ediacaran.sales.entity.Product;
+import br.com.uoutec.community.ediacaran.sales.entity.ProductMetadata;
+import br.com.uoutec.community.ediacaran.sales.entity.ProductSearchAttributeFilter;
+import br.com.uoutec.community.ediacaran.sales.entity.ProductSearchFilter;
+import br.com.uoutec.community.ediacaran.sales.registry.ProductMetadataRegistry;
+import br.com.uoutec.community.ediacaran.system.util.SecretUtil;
+import br.com.uoutec.ediacaran.core.plugins.EntityContextPlugin;
+import br.com.uoutec.pub.entity.AbstractPubEntity;
+import br.com.uoutec.pub.entity.IdValidation;
+
+public class ProductSearchFilterPubEntity extends AbstractPubEntity<ProductSearchFilter> {
+
+	private static final long serialVersionUID = 6527154894335305698L;
+
+	@Transient
+	private Integer id;
+	
+	@NotNull(groups = IdValidation.class)
+	private String protectedID;
+	
+	@Basic(mappingType = MappingTypes.OBJECT)
+	private List<ProductSearchAttributeFilterPubEntity> filters;
+	
+	@Override
+	protected void preRebuild(ProductSearchFilter instance, boolean reload, boolean override, boolean validate) {
+		try {
+			this.id = Integer.parseInt(SecretUtil.toID(this.protectedID));
+		}
+		catch(Throwable ex){
+			this.id = 0;
+		}
+	}
+	
+	@Override
+	protected boolean isEqualId(ProductSearchFilter instance) throws Throwable {
+		return false;
+	}
+
+	@Override
+	protected boolean hasId(ProductSearchFilter instance) throws Throwable {
+		return false;
+	}
+
+	@Override
+	protected ProductSearchFilter reloadEntity() throws Throwable {
+		ProductMetadataRegistry productMetadataRegistry = EntityContextPlugin.getEntity(ProductMetadataRegistry.class);
+		ProductMetadata productMetadata = productMetadataRegistry.findProductMetadataById(this.id);
+		
+		ProductSearchFilter productSearchFilter = new ProductSearchFilter();
+		productSearchFilter.setProductMetadata(productMetadata);
+		return productSearchFilter;
+	}
+
+	@Override
+	protected void throwReloadEntityFail() throws Throwable {
+	}
+
+	@Override
+	protected ProductSearchFilter createNewInstance() throws Throwable {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	protected void copyTo(ProductSearchFilter o, boolean reload, boolean override,
+			boolean validate) throws Throwable {
+		
+		if(this.filters != null) {
+			Set<ProductSearchAttributeFilter> attributeFilters = new HashSet<>();
+			for(ProductSearchAttributeFilterPubEntity x: this.filters) {
+				attributeFilters.add(x.rebuild(true,true,true));
+			}
+		}
+		
+	}
+	
+}
