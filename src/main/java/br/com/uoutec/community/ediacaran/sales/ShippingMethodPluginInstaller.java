eab4192898ae7b285847b2836fd9d6cf05b75014
Brandao <Brandao@git.uoutec.com.br>
2026-09-19 20:53:49 -0300

    default

--- src/main/java/br/com/uoutec/community/ediacaran/sales/ShippingMethodPluginInstaller.java
+++ src/main/java/br/com/uoutec/community/ediacaran/sales/ShippingMethodPluginInstaller.java
@@ -1,15 +1,5 @@
 package br.com.uoutec.community.ediacaran.sales;
 
-import br.com.uoutec.community.ediacaran.sales.entity.Shipping;
-import br.com.uoutec.community.ediacaran.sales.payment.ElectronicShippingMethod;
-import br.com.uoutec.community.ediacaran.sales.payment.PaymentGatewayRegistry;
-import br.com.uoutec.community.ediacaran.sales.pub.entity.ShippingPubEntity;
-import br.com.uoutec.community.ediacaran.sales.shipping.ElectronicShipping;
-import br.com.uoutec.community.ediacaran.sales.shipping.ElectronicShippingPubEntity;
-import br.com.uoutec.community.ediacaran.sales.shipping.ShippingMethodRegistry;
-import br.com.uoutec.community.ediacaran.system.entity.EntityInheritanceManager;
-import br.com.uoutec.ediacaran.core.plugins.EntityContextPlugin;
-
 public class ShippingMethodPluginInstaller {
 
 	public ShippingMethodPluginInstaller() {
@@ -17,22 +7,22 @@ public class ShippingMethodPluginInstaller {
 	
 	public void install() throws Throwable {
 		
-		ShippingMethodRegistry smr = EntityContextPlugin.getEntity(ShippingMethodRegistry.class);
-		smr.registerShippingMethod(new ElectronicShippingMethod());
+		//ShippingMethodRegistry smr = EntityContextPlugin.getEntity(ShippingMethodRegistry.class);
+		//smr.registerShippingMethod(new ElectronicShippingMethod());
 		
-		EntityInheritanceManager eiu = EntityContextPlugin.getEntity(EntityInheritanceManager.class);
-		eiu.register(ElectronicShippingPubEntity.class, "electronic", ShippingPubEntity.class);  
-		eiu.register(ElectronicShipping.class, "electronic", Shipping.class);  
+		//EntityInheritanceManager eiu = EntityContextPlugin.getEntity(EntityInheritanceManager.class);
+		//eiu.register(ElectronicShippingPubEntity.class, "electronic", ShippingPubEntity.class);  
+		//eiu.register(ElectronicShipping.class, "electronic", Shipping.class);  
 	}
 	
 	public void uninstall() throws Throwable {
 		
-		PaymentGatewayRegistry pgr = EntityContextPlugin.getEntity(PaymentGatewayRegistry.class);
-		pgr.removePaymentGateway("electronic");
+		//PaymentGatewayRegistry pgr = EntityContextPlugin.getEntity(PaymentGatewayRegistry.class);
+		//pgr.removePaymentGateway("electronic");
 		
-		EntityInheritanceManager eiu = EntityContextPlugin.getEntity(EntityInheritanceManager.class);
-		eiu.remove("electronic", ShippingPubEntity.class);  
-		eiu.remove("electronic", Shipping.class);  
+		//EntityInheritanceManager eiu = EntityContextPlugin.getEntity(EntityInheritanceManager.class);
+		//eiu.remove("electronic", ShippingPubEntity.class);  
+		//eiu.remove("electronic", Shipping.class);  
 	}
 	
 }
